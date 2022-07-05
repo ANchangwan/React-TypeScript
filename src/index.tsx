7089d68916d0e042130af8e29b1564e2f2d5b8cb
An chang wan <ssdii66@gmail.com>
2022-07-06 00:36:18 +0900

    home style + link 추가

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
-import { lightTheme, darkTheme } from "./theme";
+import { theme } from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
 root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
+  <div>
+    <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>
+  </div>
 );
